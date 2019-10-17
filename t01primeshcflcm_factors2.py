f48fa68d1f6929fc77392cc1c4bda38f4086cea6
davidmiao3306 <53067703+davidmiao3306@users.noreply.github.com>
2019-10-17 13:52:40 +0800

    Update t01primeshcflcm_factors2.py

--- t01primeshcflcm_factors2.py
+++ t01primeshcflcm_factors2.py
@@ -1,6 +1,9 @@
 # Generate a list containing factors of num.
 #indicate variable "num"
 num = 34
-#extract number 1 to "num" using "i"
-range
+#extract number 1 to "num" using "i" and "for" circle
+for i in range(1, num+1):  
+  if num % i == 0:
+    print(i, end=' ')
+
 
